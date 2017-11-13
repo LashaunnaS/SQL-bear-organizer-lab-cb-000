3e97d8575aa54a10d2f3aa9061427c36490819bd
Lashaunna Samuels <shaunna191@hotmail.com>
2017-11-13 12:10:20 -0500

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears VALUES (
+  1, 'Mr. Chocolate', 29, 'M', 'Brown', 'Smooth', 0);
+INSERT INTO bears VALUES (
+  2, 'Rowdy', 14, 'M', 'Tan', 'rowdy', 1);
+INSERT INTO bears VALUES (
+  3, 'Tabitha', 16, 'F', 'Brown', 'shy', 1);
+INSERT INTO bears VALUES (
+  4, 'Sergeant Brown', 46, 'M', 'Black', 'serious', 1);
+INSERT INTO bears VALUES (
+  5, 'Melissa', 19, 'F', 'Brown', 'sad', 1);
+INSERT INTO bears VALUES (
+  6, 'Grinch', 27, 'M', 'Green', 'Grumpy', 0);
+INSERT INTO bears VALUES (
+  7, 'Wendy', 31, 'F', 'White', 'Whimsical', 1);
+INSERT INTO bears VALUES (
+  8, Null, 40, 'M', 'White', 'Dreadful', 1);

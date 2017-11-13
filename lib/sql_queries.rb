670fb3f3d45a3abb62d5bac119810df077121829
Lashaunna Samuels <shaunna191@hotmail.com>
2017-11-13 12:11:30 -0500

    Done

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'F';"
+  "SELECT bear.name, bear.age FROM bears WHERE bear.gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name;"
+  "SELECT bear.name FROM bears ORDER BY bear.name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
+  "SELECT bear.name, bear.age FROM bears WHERE bear.alive = 1 ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, MAX(age) FROM bears order by age;"
+  "SELECT bear.name, MAX(bear.age) FROM bears order by bear.age;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, Min(age) FROM bears order by age;"
+  "SELECT bear.name, Min(bear.age) FROM bears order by bear.age;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY COLOR ORDER BY COUNT(*) DESC LIMIT 1;"
+  "SELECT bear.color, COUNT(bear.color) FROM bears GROUP BY COLOR ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT count(temperament) FROM bears WHERE temperament = 'goofy';"
+  "SELECT count(bear.temperament) FROM bears WHERE bear.temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name IS NULL;"
+  "SELECT * FROM bears WHERE bear.name IS NULL;"
 end
